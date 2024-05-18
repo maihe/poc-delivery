9fbdbb7135b2f6ff2cabe14917a387381213962e
maihe <maiaraoliveira.dejesus@gmail.com>
2024-05-18 00:06:54 -0300

    testing destination API

--- src/main/java/com/maihe/pocdelivery/runner/DirectionsApiTest.java
+++ src/main/java/com/maihe/pocdelivery/runner/DirectionsApiTest.java
@@ -1,6 +1,7 @@
 package com.maihe.pocdelivery.runner;
 
 import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsLeg;
 import com.google.maps.model.DirectionsResult;
 import com.maihe.pocdelivery.service.GoogleMapsDirectionsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class DirectionsApiTest implements CommandLineRunner {
@@ -18,23 +21,95 @@ public class DirectionsApiTest implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         String origin = "rua olimpia de almeida prado, 27, SÃO PAULO";
-        String destination = "Rua Lopes de oliveira 487, São Paulo";
+        String destination = "Faculdade FIAP, São Paulo";
 
+        List<String> waypoints = new ArrayList<>();
+        String waypoint1 = "Hospiral Samaritano, São Paulo";
+        String waypoint2 = "Estadio Pacaembu, São Paulo";
+
+        waypoints.add(waypoint1);
+        System.out.println("\n ------------- Calcule Rota Normal -------------");
+        attemptOne(origin, destination);
+        System.out.println("\n ------------- Calcule com 1 Parada -------------");
+        attemptOne(origin, destination, waypoints);
+        System.out.println("\n ------------- Calcule com 2 Parada -------------");
+        waypoints.add(waypoint2);
+        attemptOne(origin, destination, waypoints);
+
+        System.out.println("\n ------------- Calcule com 2 Parada B -------------");
+        List<String> waypoints2 = new ArrayList<>();
+        waypoints2.add(waypoint2);
+        attemptOne(origin, destination, waypoints2);
+    }
+
+    private void attemptOne(String origin, String destination) {
         try {
             DirectionsResult result = directionsService.getDirections(origin, destination);
 
             if (result.routes != null && result.routes.length > 0) {
                 System.out.println("Rota encontrada:");
                 System.out.println("Distância: " + result.routes[0].legs[0].distance.humanReadable);
-                System.out.println("Tempo estimado: " + result.routes[0].legs[0].duration.humanReadable);
-                System.out.println("Data chegada: " + result.routes[0].legs[0].arrivalTime.toLocalDateTime().toString());
-                // ... (Outras informações da rota)
+                System.out.println("Tempo estimado com transito: " + result.routes[0].legs[0].durationInTraffic.humanReadable);
+                //System.out.println("Data chegada: " + result.routes[0].legs[0].arrivalTime.toLocalDateTime().toString());
             } else {
                 System.out.println("Nenhuma rota encontrada.");
             }
         } catch (IOException | InterruptedException | ApiException e) {
             System.err.println("Erro ao calcular a rota: " + e.getMessage());
         }
+    }
+
+    private void attemptOne(String origin, String destination,  List<String> waypoint) {
+
+        try {
+            DirectionsResult result = directionsService.getDirections(origin, destination, waypoint);
+
+            if (result.routes != null && result.routes.length > 0) {
+                System.out.println("Rota encontrada:");
+
+                int legNumber=1;
+                long totalDuration=0;
+                long distance=0;
+                for (DirectionsLeg leg : result.routes[0].legs){
+                    System.out.println(legNumber + " Distância do trecho: " + leg.distance);
+                    System.out.println(legNumber + " Tempo estimado trecho: " + leg.duration.humanReadable);
+                    legNumber++;
+                    totalDuration += leg.duration.inSeconds;
+                    distance += leg.distance.inMeters;
+                }
+                System.out.println("Distancia total: " + convertMetros(distance) + "\n");
+
+                System.out.println("Tempo estimado total: " + convertSeconds(totalDuration) + "\n");
+                //System.out.println("Data chegada: " + result.routes[0].legs[0].arrivalTime.toLocalDateTime().toString());
+            } else {
+                System.out.println("Nenhuma rota encontrada.");
+            }
+        } catch (IOException | InterruptedException | ApiException e) {
+            System.err.println("Erro ao calcular a rota: " + e.getMessage());
+        }
+    }
+
+    private String convertSeconds(long totalDuration){
+        long seconds,minutes,hours,days;
+
+        seconds = totalDuration % 60;
+        totalDuration /= 60;
+        minutes = totalDuration % 60;
+        totalDuration /= 60;
+        hours = totalDuration % 24;
+        totalDuration /= 24;
+        days = totalDuration;
+
+        return days+"d "+hours+"h "+minutes+"m "+seconds+"s";
+    }
+    private String convertMetros(long distance){
+        long km,metros;
+
+        metros = distance % 1000;
+        distance /= 1000;
+        km = distance;
+
+        return km+"km "+metros+"m";
     }
 }
 
--- src/main/java/com/maihe/pocdelivery/service/GoogleMapsDirectionsService.java
+++ src/main/java/com/maihe/pocdelivery/service/GoogleMapsDirectionsService.java
@@ -1,16 +1,16 @@
 package com.maihe.pocdelivery.service;
 
 import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
 import com.google.maps.GeoApiContext;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.DirectionsResult;
-import com.google.maps.model.LatLng;
 import com.google.maps.model.TravelMode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.time.Instant;
+import java.util.List;
 
 @Service
 public class GoogleMapsDirectionsService {
@@ -19,23 +19,44 @@ public class GoogleMapsDirectionsService {
     private String apiKey;
 
     public DirectionsResult getDirections(String origin, String destination) throws IOException, InterruptedException, ApiException {
-        return getDirections(origin, destination, Instant.now());
-    }
-
-    public DirectionsResult getDirections(String origin, String destination, Instant departure) throws IOException, InterruptedException, ApiException {
         GeoApiContext context = new GeoApiContext.Builder()
                 .apiKey(apiKey)
                 .build();
-
-        //LatLng originLatLng = new LatLng(getLatitude(origin), getLongitude(origin));
-        //LatLng destinationLatLng = new LatLng(getLatitude(destination), getLongitude(destination));
-
-        return DirectionsApi.newRequest(context)
+        DirectionsResult directionsResult = DirectionsApi.newRequest(context)
                 .origin(origin)
                 .destination(destination)
                 .mode(TravelMode.DRIVING)
-                .departureTime(departure)
+                .departureTimeNow()
                 .await();
+        context.shutdown();
+        return directionsResult;
+    }
+
+    public DirectionsResult getDirections(String origin, String destination, List<String> waypoints) throws IOException, InterruptedException, ApiException {
+        try {
+            GeoApiContext context = new GeoApiContext.Builder()
+                    .apiKey(apiKey)
+                    .build();
+
+            DirectionsApiRequest directionsApiRequest = DirectionsApi.newRequest(context)
+                    .origin(origin)
+                    .destination(destination)
+                    .mode(TravelMode.DRIVING)
+                    .departureTimeNow();
+
+            if (waypoints != null && !waypoints.isEmpty()) {
+                for (String waypoint : waypoints) {
+                    directionsApiRequest.waypoints(waypoint);
+                }
+            }
+            DirectionsResult directionsResult = directionsApiRequest.await();
+            context.shutdown();
+            return directionsResult;
+        }catch (Exception e){
+            System.out.println("error: " + e.getMessage());
+            throw e;
+        }
+
     }
 
    /* private double getLatitude(String location) {
